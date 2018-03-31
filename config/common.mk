4f243a6598599185b2513bf26a8638695f142c83
Ded_Boi <dhanush911gtr@gmail.com>
2018-03-31 14:04:25 +0530

    [2/2] Add 'GoogleSans' font family (#1)
    Change-Id: I15e7760d2b7b30cbfedc0dd3278ea20f924adc25
    Signed-off-by: Jagrav Naik <jagravnaik0@gmail.com>
    Signed-off-by: Ded_Boi <dhanush911gtr@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -368,3 +368,12 @@ endif
 
 -include $(WORKSPACE)/build_env/image-auto-bits.mk
 -include vendor/lineage/config/partner_gms.mk
+
+# Fonts
+PRODUCT_COPY_FILES += \
+    vendor/lineage/fonts/GoogleSans-Regular.ttf:system/fonts/GoogleSans-Regular.ttf \
+    vendor/lineage/fonts/GoogleSans-Medium.ttf:system/fonts/GoogleSans-Medium.ttf \
+    vendor/lineage/fonts/GoogleSans-MediumItalic.ttf:system/fonts/GoogleSans-MediumItalic.ttf \
+    vendor/lineage/fonts/GoogleSans-Italic.ttf:system/fonts/GoogleSans-Italic.ttf \
+    vendor/lineage/fonts/GoogleSans-Bold.ttf:system/fonts/GoogleSans-Bold.ttf \
+    vendor/lineage/fonts/GoogleSans-BoldItalic.ttf:system/fonts/GoogleSans-BoldItalic.ttf
