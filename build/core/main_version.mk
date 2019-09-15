fc9b2a95da21980cd667d85294a7ef45e25d5505
Pranav Vashi <neobuddy89@gmail.com>
2019-09-15 23:52:51 +0530

    Make lineage great again
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: ShanuDey <sd4shanudey@gmail.com>

--- build/core/main_version.mk
+++ build/core/main_version.mk
@@ -4,17 +4,12 @@ ADDITIONAL_BUILD_PROPERTIES += \
     ro.build.fingerprint=$(BUILD_FINGERPRINT)
 endif
 
-# LineageOS System Version
+# LineageX System Version
 ADDITIONAL_BUILD_PROPERTIES += \
-    ro.lineage.version=$(LINEAGE_VERSION) \
-    ro.lineage.releasetype=$(LINEAGE_BUILDTYPE) \
-    ro.lineage.build.version=$(PRODUCT_VERSION_MAJOR).$(PRODUCT_VERSION_MINOR) \
-    ro.modversion=$(LINEAGE_VERSION) \
-    ro.lineagelegal.url=https://lineageos.org/legal
-
-# LineageOS Platform Display Version
-ADDITIONAL_BUILD_PROPERTIES += \
-    ro.lineage.display.version=$(LINEAGE_DISPLAY_VERSION)
+    ro.lineagex.build.version=$(LINEAGE_VERSION) \
+    ro.lineagex.display.version=$(LINEAGE_DISPLAY_VERSION) \
+    ro.lineagex.version=$(PRODUCT_VERSION_MAJOR).$(PRODUCT_VERSION_MINOR) \
+    ro.modversion=$(LINEAGEX_VERSION)
 
 # LineageOS Platform SDK Version
 ADDITIONAL_BUILD_PROPERTIES += \
