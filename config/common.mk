AICP/vendor_aicp

vendor_aicp: Add Ubuntu font overlay
This adds support for custom fonts starting with Ubuntu,
making it an opt-in overlay.

AICP: fixed find-copy-subdir-files args
      build actual FontUbuntuOverlay

Change-Id: Ie39820fbb2e65a752a473dca020cf127d7a819dd
Signed-off-by: Ali B <abittin@gmail.com>
Signed-off-by: doc HD <doc.divxm@gmail.com>

config/common.mk
@@ -73,6 +73,11 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     vendor/aicp/config/permissions/lineage-hiddenapi-package-whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/lineage-hiddenapi-package-whitelist.xml
 
+# Fonts
+PRODUCT_COPY_FILES += \
+    $(call find-copy-subdir-files,*,vendor/aicp/prebuilt/common/fonts,$(TARGET_COPY_OUT_PRODUCT)/fonts) \
+    vendor/aicp/prebuilt/common/etc/fonts_customization.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/fonts_customization.xml
+
 # Power whitelist
 PRODUCT_COPY_FILES += \
     vendor/aicp/config/permissions/aicp-power-whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/aicp-power-whitelist.xml

Lineage-X/android_vendor_lineage

Signed-off-by: Sujit Roy <kumarsujitroy@gmail.com>

config/common.mk
@@ -52,7 +52,13 @@ PRODUCT_COPY_FILES += \
 $(foreach f,$(wildcard vendor/lineage/prebuilt/common/etc/init/*.rc),\
 	$(eval PRODUCT_COPY_FILES += $(f):$(TARGET_COPY_OUT_SYSTEM)/etc/init/$(notdir $f)))
 
+# Fonts
+PRODUCT_COPY_FILES += \
+    $(call find-copy-subdir-files,*,vendor/lineage/prebuilt/common/fonts,$(TARGET_COPY_OUT_PRODUCT)/fonts) \
+    vendor/lineage/prebuilt/common/etc/fonts_customization.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/fonts_customization.xml
+
 # Copy over added mimetype supported in libcore.net.MimeUtils
+# Power whitelist
 PRODUCT_COPY_FILES += \
     vendor/lineage/prebuilt/common/lib/content-types.properties:$(TARGET_COPY_OUT_SYSTEM)/lib/content-types.properties
 
@@ -241,6 +247,10 @@ PRODUCT_PACKAGES += \
 # These packages are excluded from user builds
 PRODUCT_PACKAGES_DEBUG += \
     procmem
+
+# font packages
+PRODUCT_PACKAGES += \
+    FontUbuntuOverlay
 
 # Conditionally build in su
 ifneq ($(TARGET_BUILD_VARIANT),user)