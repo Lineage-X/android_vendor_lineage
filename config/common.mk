Project-Xtended/vendor_xtended

[2/2] Add 'GoogleSans' font family (#1)
Change-Id: I15e7760d2b7b30cbfedc0dd3278ea20f924adc25
Signed-off-by: Jagrav Naik <jagravnaik0@gmail.com>
Signed-off-by: Ded_Boi <dhanush911gtr@gmail.com>

config/common.mk
@@ -212,3 +212,12 @@ PRODUCT_PROPERTY_OVERRIDES += \
 include vendor/xtended/google/GoogleAudio.mk
 
 EXTENDED_POST_PROCESS_PROPS := vendor/xtended/tools/xtended_process_props.py
+
+# Fonts
+PRODUCT_COPY_FILES += \
+    vendor/xtended/fonts/GoogleSans-Regular.ttf:system/fonts/GoogleSans-Regular.ttf \
+    vendor/xtended/fonts/GoogleSans-Medium.ttf:system/fonts/GoogleSans-Medium.ttf \
+    vendor/xtended/fonts/GoogleSans-MediumItalic.ttf:system/fonts/GoogleSans-MediumItalic.ttf \
+    vendor/xtended/fonts/GoogleSans-Italic.ttf:system/fonts/GoogleSans-Italic.ttf \
+    vendor/xtended/fonts/GoogleSans-Bold.ttf:system/fonts/GoogleSans-Bold.ttf \
+    vendor/xtended/fonts/GoogleSans-BoldItalic.ttf:system/fonts/GoogleSans-BoldItalic.ttf

Lineage-X/android_vendor_lineage

config/common.mk
@@ -311,3 +311,12 @@ endif
 
 -include $(WORKSPACE)/build_env/image-auto-bits.mk
 -include vendor/lineage/config/partner_gms.mk
+
+# Fonts
+PRODUCT_COPY_FILES += \
+    vendor/lineage/fonts/GoogleSans-Regular.ttf:system/fonts/GoogleSans-Regular.ttf \
+    vendor/lineage/fonts/GoogleSans-Medium.ttf:system/fonts/GoogleSans-Medium.ttf \
+    vendor/lineage/fonts/GoogleSans-MediumItalic.ttf:system/fonts/GoogleSans-MediumItalic.ttf \
+    vendor/lineage/fonts/GoogleSans-Italic.ttf:system/fonts/GoogleSans-Italic.ttf \
+    vendor/lineage/fonts/GoogleSans-Bold.ttf:system/fonts/GoogleSans-Bold.ttf \
+    vendor/lineage/fonts/GoogleSans-BoldItalic.ttf:system/fonts/GoogleSans-BoldItalic.ttf